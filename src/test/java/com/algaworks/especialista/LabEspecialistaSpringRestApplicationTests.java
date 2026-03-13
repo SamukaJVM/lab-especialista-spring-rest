a73476b5d3d9e30bef7678ff613299dd52b0a964
samuel <samuka.jvm@gmail.com>
2026-03-13 09:41:02 -0400

    Initial commit

---
+++ src/test/java/com/algaworks/especialista/LabEspecialistaSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.especialista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabEspecialistaSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
